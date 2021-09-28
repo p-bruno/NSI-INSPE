69c0b792ec47f4b6533c50da6f48d43536a7fce2
BaguetttesChinoises <brunopaul1412@gmail.com>
2021-09-28 16:35:38 +0200

    Create trust.py

---
+++ trust.py
@@ -0,0 +1,12 @@
+import os
+g = open("trust.sh","w")
+g.write("#!/bin/bash\n")
+fichier=[]
+
+for root, dirs, files in os.walk(os.getcwd()): 
+    for i in files: 
+        f = os.path.join(root, i)
+        if f[-6:] == ".ipynb":
+            g.write("jupyter trust {}\n".format(f))
+  
+g.close()
